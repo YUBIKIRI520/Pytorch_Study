363f38f9e784e832ed26f80d0b6f1d4b6cf6ec33
YUBIKIRI520 <648308255@qq.com>
2020-04-20 23:14:12 +0800

    Upload

---
+++ batch.py
@@ -0,0 +1,25 @@
+import torch
+import torch.utils.data as Data
+
+BATCH_SIZE = 5
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
+
+torch_dataset = Data.TensorDataset(x, y)
+# 使训练变为一小批一小批
+loader = Data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=2
+)
+
+
+def showBatch():
+    for epoch in range(3):
+        for step, (batch_x, batch_y) in enumerate(loader):
+            # train your data...
+            print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+                  batch_x.numpy(), '| batch y: ', batch_y.numpy())
+
+showBatch()
---
+++ optimizer.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional
+import torch.utils.data as Data
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+# hyper parameter
+LR = 0.01
+BATCH_SIZE = 32
+EPOCH = 12
+
+x = torch.unsqueeze(torch.linspace(-1, 1, 1000), dim=1)
+y = x.pow(2) + 0.1 * torch.normal(torch.zeros(*x.size()))
+
+# 打印一下看看
+plt.scatter(x.data.numpy(), y.data.numpy())
+plt.show()
+
+# 用于github测试
